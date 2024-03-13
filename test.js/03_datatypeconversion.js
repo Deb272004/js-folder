d911303f061204a6a19992e71b9f67ba613acb68
deblina <deblina.gupta2004@gmail.com>
2024-03-14 00:06:32 +0530

    conversion of datatypes

---
+++ test.js/03_datatypeconversion.js
@@ -0,0 +1,24 @@
+// let score = "deblina"  //undefined   // string //null // 33 //boolean
+
+// const {score} = req.body
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+//"33" => 33
+// "33qbc"=> NaN
+// true => 1 and false=> 0
+
+
+// let isloggedIn = 1  //"" => false // "string" => true // 0 => false
+// let booleanIsloggedIn = Boolean(isloggedIn)
+// console.log(booleanIsloggedIn)
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
