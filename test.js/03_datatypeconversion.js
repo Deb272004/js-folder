52b6935de7c6a8d234a9155f8c451fa3a522bec9
deblina <deblina.gupta2004@gmail.com>
2024-03-14 01:05:30 +0530

    operation

--- test.js/03_datatypeconversion.js
+++ test.js/03_datatypeconversion.js
@@ -20,5 +20,58 @@
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+
+
+                               // operations
+
+
+
+
+let value = 3
+let negvalue = -value
+// console.log(negvalue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2) //power
+// console.log(2/3)
+// console.log(2%3)  //used in cryptography and algorithm
+
+
+let str1 = "hello"
+let str2 = " deblina"
+
+let str3 = str1 + str2
+console.log(str3) ;   // concatenation of two strings
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);  // if first number is string treats the next number as strings aswell
+console.log(1 + 2 + "2");  //if strig is at last firstly doea the operation than concatenate with the string
+
+
+console.log(3 + 4 * 5 % 3);  //not a good practice to use messy codes
+
+
+
+
+
+console.log(+true) // => 1
+console.log(+"");  // => 0
+
+
+let num1,num2,num3
+num1 = num2 = num3 = 2 + 2   // generally not PPRECITAED IN COMPANIES
+
+ let gameCounter = 100
+ gameCounter++     //postfix increment // prefix increament // gives the same result but are different 
+ console.log(gameCounter);  
+
+
+
+
+
